a8eb1e4ae4711f16ef3a0ab0ade0c90429b88da5
arthurhzna <arthurhozana123@gmail.com>
2026-02-04 22:26:18 +0700

    leetcode day 4 aaaa ikuzoo

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
-# I'm sorry, Mom. I just purchased NeetCode :)
-
 ![9 to 5 Leetcode Every Night](nevergiveup.png)
 
 ## The Grind Never Stops
 
-Working 9-5 every day, then coming home to practice LeetCode every night. The journey from office worker to coding master - one problem at a time. 💪
+Working 9-5 every day, then coming home to practice LeetCode every night. one problem at a time. 💪
--- test.py
+++ test.py
@@ -26,32 +26,38 @@ from typing import List
 #         return False
 
 
+# class Solution:
+#     def isPalindrome(self, s: str) -> bool:
+#         left = 0
+#         right = len(s)-1
+
+#         while left < right:
+#             # print(f"left -- {s[left]} and right -- {s[right]}")
+#             while left < right and not self.is_alphanumeric(s[left]):
+#                 print(f"left -- {s[left]} is not alphanumeric")
+#                 left += 1 
+#             while right > left and not self.is_alphanumeric(s[right]):
+#                 print(f"right -- {s[right]} is not alphanumeric")
+#                 right -= 1 
+#             print(f"left -- {s[left]} and right -- {s[right]}")
+#             if s[left].lower() != s[right].lower():
+#                 return False
+#             left += 1
+#             right -= 1
+#             # print(f"after left -- {s[left]} and right -- {s[right]}")
+#         return True
+
+#     def is_alphanumeric(self, c:str) -> bool:
+#         return (ord('A') <= ord(c) <= ord('Z'))or (ord('a') <= ord(c) <= ord('z')) or (ord('0') <= ord(c) <= ord('9'))
+
+
 class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        left = 0
-        right = len(s)-1
-
-        while left < right:
-            # print(f"left -- {s[left]} and right -- {s[right]}")
-            while left < right and not self.is_alphanumeric(s[left]):
-                print(f"left -- {s[left]} is not alphanumeric")
-                left += 1 
-            while right > left and not self.is_alphanumeric(s[right]):
-                print(f"right -- {s[right]} is not alphanumeric")
-                right -= 1 
-            print(f"left -- {s[left]} and right -- {s[right]}")
-            if s[left].lower() != s[right].lower():
-                return False
-            left += 1
-            right -= 1
-            # print(f"after left -- {s[left]} and right -- {s[right]}")
-        return True
-
-    def is_alphanumeric(self, c:str) -> bool:
-        return (ord('A') <= ord(c) <= ord('Z'))or (ord('a') <= ord(c) <= ord('z')) or (ord('0') <= ord(c) <= ord('9'))
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        #sort + check double variable x, x + y + z , then x and y using two pointers
+        
 
 
         
 
 test = Solution()
-print(test.isPalindrome("A man, a plan, a canal: Panama"))
+print(test.threeSum([-1,0,1,2,-1,-4]))
