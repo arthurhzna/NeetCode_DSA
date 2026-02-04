2c1cdd1a6d8f34ba72e197d0557fadbca63b0a89
arthurhzna <arthurhozana123@gmail.com>
2026-02-04 21:40:35 +0700

    leetcode day 4 aaaa ikuzoo

--- test.py
+++ test.py
@@ -1,18 +1,57 @@
 from typing import List
 
-class Solution:
-    def longestConsecutive(self, nums: List[int]) -> int:
-        nums_set = set(nums)
-        longest = 0
+# class Solution:
+#     def longestConsecutive(self, nums: List[int]) -> int:
+#         nums_set = set(nums)
+#         longest = 0
         
-        for num in nums_set:
-            if num-1 not in nums_set:
-                temp_longest = 1
-                while(num + temp_longest) in nums_set:
-                    temp_longest += 1
-                longest = max(longest, temp_longest)
-        return longest
+#         for num in nums_set:
+#             if num-1 not in nums_set:
+#                 temp_longest = 1
+#                 while(num + temp_longest) in nums_set:
+#                     temp_longest += 1
+#                 longest = max(longest, temp_longest)
+#         return longest
 
+# class Solution:
+#     def isPalindrome(self, s: str) -> bool:
+#         s =s.lower()
+#         print(f"s before -- {s}")
+#         for x in s:
+#             if x.isalnum() is False:
+#                 s = s.replace(x, "")
+#         print(f"s after -- {s}")
+#         if s == s[::-1]:
+#             return True
+#         return False
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left = 0
+        right = len(s)-1
+
+        while left < right:
+            # print(f"left -- {s[left]} and right -- {s[right]}")
+            while left < right and not self.is_alphanumeric(s[left]):
+                print(f"left -- {s[left]} is not alphanumeric")
+                left += 1 
+            while right > left and not self.is_alphanumeric(s[right]):
+                print(f"right -- {s[right]} is not alphanumeric")
+                right -= 1 
+            print(f"left -- {s[left]} and right -- {s[right]}")
+            if s[left].lower() != s[right].lower():
+                return False
+            left += 1
+            right -= 1
+            # print(f"after left -- {s[left]} and right -- {s[right]}")
+        return True
+
+    def is_alphanumeric(self, c:str) -> bool:
+        return (ord('A') <= ord(c) <= ord('Z'))or (ord('a') <= ord(c) <= ord('z')) or (ord('0') <= ord(c) <= ord('9'))
+
+
+        
 
 test = Solution()
-print(test.longestConsecutive([100,4,200,1,3,2]))
+print(test.isPalindrome("A man, a plan, a canal: Panama"))
