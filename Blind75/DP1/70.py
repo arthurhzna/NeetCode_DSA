8c9908b629cff601330799a83354e2fec6118405
Arthur Hozanna <90093341+arthurhzna@users.noreply.github.com>
2026-03-16 22:34:57 +0700

    hmm

--- Blind75/DP1/70.py
+++ Blind75/DP1/70.py
@@ -1,10 +1,7 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        one, two = 1, 1
-
-        for i in range(n - 1):
-            temp = one
-            one = one + two
-            two = temp
-
-        return one
+        sqrt5 = math.sqrt(5)
+        phi = (1 + sqrt5) / 2
+        psi = (1 - sqrt5) / 2
+        n += 1
+        return round((phi**n - psi**n) / sqrt5)
